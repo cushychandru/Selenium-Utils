85828e29aa7e8f7e9c4cc5ef9dfee11de58a06a3
Chandrashekhar Gouda <cushychandru@gmail.com>
2019-12-04 13:20:28 +0530

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -31,7 +31,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-import com.iboarm.constants.Constant;
+import com.yourapp.constants.Constant;
 
 import report_genarator.Reporter;
 
